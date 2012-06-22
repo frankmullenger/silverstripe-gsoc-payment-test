38c09729047259d50a993aef9c14911b5fda2aeb
Ryan Dao <daoduyducduong@gmail.com>
2012-06-23 04:56:32 +0800

    Modify to suite new API

--- code/PaymentTestPage.php
+++ code/PaymentTestPage.php
@@ -25,23 +25,17 @@ class PaymentTestPage_Controller extends Page_Controller {
    * Get the order form for processing a dummy payment
    */
   function OrderForm() {
+    $fields = new FieldList;
 
-    $fields = new FieldList();
+    // Create a dropdown select field for choosing gateway
+    $supported_methods = Payment_Controller::get_supported_methods();
+    $fields->push(new DropDownField(
+      'payment_controller', 
+      'Select Payment Method', 
+      $supported_methods
+    ));
 
-    //$dummyPayment = singleton('Dummy_Payment');
-    //SS_Log::log(new Exception(print_r($dummyPayment, true)), SS_Log::NOTICE);
-
-    //TODO For each payment method that is enabled get the form fields
-    //old way was Payment::get_combined_form_fields() - is there a better way now?
-    //maybe we can use DI for this also?
-    $paymentController = singleton('Payment_Controller');
-
-    $paymentFields = $paymentController->getFormFields();
-    //SS_Log::log(new Exception(print_r($paymentFields->map(), true)), SS_Log::NOTICE);
-
-    //TODO should we wrap payment fields in a CompositeField so we can just
-    //$fields->push($paymentFields);
-    //or is that too restrictive from a frontend perspective?
+    $paymentFields = Payment_Controller::get_combined_form_fields();
 
     if ($paymentFields && $paymentFields->exists()) foreach ($paymentFields as $paymentField) {
       $fields->push($paymentField);
